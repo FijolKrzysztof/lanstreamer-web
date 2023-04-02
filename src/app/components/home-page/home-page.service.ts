1f7fc744dbc93c4126be1cefa9a401263efbc8c8
FijolKrzysztof <xkrzysztoffijol@gmail.com>
2023-04-02 14:55:22 +0200

    Home page

---
+++ src/app/components/home-page/home-page.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {DownloadType} from "./home-page.component";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomePageService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getDownloadLink(type: DownloadType): Observable<string> {
+    return this.http.get(`${environment.serverURL}/api/main/download/${type}`, {responseType: 'text'});
+  }
+}
