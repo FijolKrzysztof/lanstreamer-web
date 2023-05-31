dd9fc34ce0e2a282853a0295c1bbf4ea78d83ad8
FijolKrzysztof <xkrzysztoffijol@gmail.com>
2023-05-31 21:08:51 +0200

    Usunięcie testów, poprawa environments

--- src/app/components/authentication/authentication.service.ts
+++ src/app/components/authentication/authentication.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {TokenInfo} from "./token-info";
+import {EnvService} from "../../env.service";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,11 @@ import {TokenInfo} from "./token-info";
 export class AuthenticationService {
   constructor(
     private http: HttpClient,
+    private envService: EnvService,
   ) { }
 
   authenticate(code: string, mail: string): Observable<void> {
-    return this.http.post<void>(`https://lanstreamer.com:5000/api/main/authorize/${code}`, {Mail: mail});
+    return this.http.post<void>(`${this.envService.baseURL}/api/main/authorize/${code}`, {Mail: mail});
   }
 
   getTokenInfo(token: string): Observable<TokenInfo> {
